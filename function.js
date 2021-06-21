30c191799ab633956f6453f22136d21eec4f7641
simpilevar <nasim.pilevar@gmail.com>
2021-06-21 04:51:00 +0430

    Add some javascript for menubar

---
+++ function.js
@@ -0,0 +1,28 @@
+
+function OpenMenu() {
+      
+    var menu = document.getElementById("modalMenu");
+    if(menu.style.display === "none") {menu.style.display = "block";
+            }
+    else{
+        menu.style.display = "none";
+    }
+
+  } 
+
+  var MenuMainHolder = document.getElementById('modalMainHolder');
+// function Myfunction()
+// {
+//     if(menu.style.display === "block"){
+//         MenuMainHolder.style.display = "grid";
+//     }
+//     else{
+//         MenuMainHolder.style.display = "none";
+//     }
+// }
+
+// while (menu.style.display === "block") {
+//     do{
+//         MenuMainHolder.style.display = "grid";
+//     }
+// }
