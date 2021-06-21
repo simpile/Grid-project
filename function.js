fb1d6caba7e92f49cb49970f4e6985556bca9ee6
simpilevar <nasim.pilevar@gmail.com>
2021-06-21 23:57:31 +0430

    add modal menu with Js and do some basic design

--- function.js
+++ function.js
@@ -3,26 +3,40 @@ function OpenMenu() {
       
     var menu = document.getElementById("modalMenu");
     if(menu.style.display === "none") {menu.style.display = "block";
+    menu.style.transition =  "5s";
             }
     else{
         menu.style.display = "none";
     }
-
+  
   } 
 
-  var MenuMainHolder = document.getElementById('modalMainHolder');
-// function Myfunction()
-// {
-//     if(menu.style.display === "block"){
-//         MenuMainHolder.style.display = "grid";
-//     }
-//     else{
-//         MenuMainHolder.style.display = "none";
-//     }
-// }
+  // var MenuMainHolder = document.getElementById('modalMainHolder');
+
+  //   if(menu.style.display == "block"){
+  //     console.log('its block');
+  //       MenuMainHolder.style.display = "grid";
+  //   }
+  //   else{
+  //       MenuMainHolder.style.display = "none";
+
 
 // while (menu.style.display === "block") {
 //     do{
 //         MenuMainHolder.style.display = "grid";
 //     }
 // }
+// ................
+// const container = document.getElementById("container");
+
+// function makeRows(rows, cols) {
+//   container.style.setProperty('--grid-rows', rows);
+//   container.style.setProperty('--grid-cols', cols);
+//   for (c = 0; c < (rows * cols); c++) {
+//     let cell = document.createElement("div");
+//     cell.innerText = (c + 1);
+//     container.appendChild(cell).className = "grid-item";
+//   };
+// };
+
+// makeRows(16, 16);
