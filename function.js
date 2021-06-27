eb0ffe5b24c6bdb93197112433a78df086b1c18d
simpilevar <nasim.pilevar@gmail.com>
2021-06-28 01:25:01 +0430

    try to add cart menu JS but there was some error

--- function.js
+++ function.js
@@ -3,40 +3,23 @@ function OpenMenu() {
       
     var menu = document.getElementById("modalMenu");
     if(menu.style.display === "none") {menu.style.display = "block";
-    menu.style.transition =  "5s";
+   
             }
     else{
         menu.style.display = "none";
     }
   
   } 
+var cartop = document.getElementsById("modalCartHolder");
 
-  // var MenuMainHolder = document.getElementById('modalMainHolder');
+  function OpenCartMenu() {
 
-  //   if(menu.style.display == "block"){
-  //     console.log('its block');
-  //       MenuMainHolder.style.display = "grid";
-  //   }
-  //   else{
-  //       MenuMainHolder.style.display = "none";
-
-
-// while (menu.style.display === "block") {
-//     do{
-//         MenuMainHolder.style.display = "grid";
-//     }
-// }
-// ................
-// const container = document.getElementById("container");
-
-// function makeRows(rows, cols) {
-//   container.style.setProperty('--grid-rows', rows);
-//   container.style.setProperty('--grid-cols', cols);
-//   for (c = 0; c < (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     cell.innerText = (c + 1);
-//     container.appendChild(cell).className = "grid-item";
-//   };
-// };
-
-// makeRows(16, 16);
+if(cartop.style.display == "none") {
+  cartop.style.display = "block";
+   
+            }
+    else{
+        cartop.style.display = "none";
+    }
+  
+  }
